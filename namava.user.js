2dc9b963467cc3ea53dcb3f2dfe2a78675da093c
Mostafa Nouri <mostafa.nouri@gmail.com>
2021-11-12 15:25:54 +0330

    Update namava.user.js
    update for the new interface

--- namava.user.js
+++ namava.user.js
@@ -1,40 +1,47 @@
-// ==UserScript==
+//// ==UserScript==
 // @name namava
-// @description 
 // @version 0.1
 // @author mostafa
-// @license MIT
 // @match *://www.namava.ir/*
 // @run-at document-end
 // @grant none
 // ==/UserScript==
 
+var tvip        = '192.168.10.110:9090'
+
+function addButtons(link, movieId) {
+  //   var foo2 = document.createTextNode(' ')
+  //   var newlink2 = link.cloneNode(false)
+  //   newlink2.href      = 'http://' + tvip + '/play1080?'+movieId
+  //   newlink2.innerHTML = 'پخش در تلویزیون (۱۰۸۰)'
+  //   link.parentNode.insertBefore(newlink2, link.nextSibling)
+  //   link.parentNode.insertBefore(foo2, link.nextSibling)
+
+  var foo = document.createTextNode(' ')
+  var newlink = link.cloneNode(false)
+  newlink.href      = 'http://' + tvip + '/play?'+movieId
+  newlink.innerHTML = 'پخش در تلویزیون'
+  link.parentNode.insertBefore(newlink, link.nextSibling)
+  link.parentNode.insertBefore(foo, link.nextSibling)
+}
+
 window.onload = function () {
-	var searchstring = 'تماشای فیلم';
-  var anchors = document.getElementsByTagName('a');
 
-  for (i=0; i<anchors.length; i++)
-  {
-      if (anchors[i].innerHTML.includes(searchstring))
-      { 
-          var movieId     = anchors[i].onclick.toString().replace (/\D+(\d+)\D+/, '$1')
-          var tvip        = '192.168.1.1'
-          
-          var foo2 = document.createTextNode(' ');
-          var newlink2 = anchors[i].cloneNode(false);
-          newlink2.href      = 'http://' + tvip + '/play1080?'+movieId;
-          newlink2.innerHTML = 'پخش در تلویزیون (۱۰۸۰)';
-    	
-          anchors[i].parentNode.insertBefore(newlink2, anchors[i].nextSibling);
-          anchors[i].parentNode.insertBefore(foo2, anchors[i].nextSibling);
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      mutation.addedNodes.forEach(function(addedNode) {
+        anchors = addedNode.getElementsByTagName('a')
+        for (i=0; i<anchors.length; i++)
+        {
+          if (anchors[i].href.includes('play.namava.ir')) {
+            addButtons(anchors[i], anchors[i].href.replace (/\D+m=(\d+)&.*/, '$1'));
+          } else if (anchors[i].href.includes('namava.ir/play')) {
+            addButtons(anchors[i], anchors[i].href.replace (/\D+(\d+)\D*/, '$1'));
+          }
+        }
+      });
+    });
+  });
 
-          var foo = document.createTextNode(' ');
-          var newlink = anchors[i].cloneNode(false);
-          newlink.href      = 'http://' + tvip + '/play?'+movieId;
-          newlink.innerHTML = 'پخش در تلویزیون';
-          anchors[i].parentNode.insertBefore(newlink, anchors[i].nextSibling);
-          anchors[i].parentNode.insertBefore(foo, anchors[i].nextSibling);
-
-      }
-  }
+  observer.observe(document.body, {childList: true, subtree: true});
 };
